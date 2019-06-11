8f066acadb5d42abb2352aa862bc2d27a05285f8
One-Lemon <qq982516861@gmail.com>
2019-06-11 18:54:38 +0800

    合并详情

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import films from './index/films'
 import cinemas from './cinema/index'
+import detail from './detail'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     films,
-    cinemas
+    cinemas,
+    detail
   }
 })
